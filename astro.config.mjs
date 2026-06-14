9f4d9902f143e7b3fb1f9f1582496a10ee8789c7
erlkim <xim7.iin@gmail.com>
2026-06-15 01:13:40 +0700

    Fix vercel adapter - direct import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import keystatic from '@keystatic/astro';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
