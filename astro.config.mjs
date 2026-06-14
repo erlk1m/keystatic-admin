778a2a216ba4529187ff77a2733abd042aba893d
erlkim <xim7.iin@gmail.com>
2026-06-15 00:46:40 +0700

    Add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import keystatic from '@keystatic/astro';
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   integrations: [keystatic(), react()],
   output: 'server',
+  adapter: cloudflare(),
 });
